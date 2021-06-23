5e79888ef92349d07d62dcd3319b087751b11bfa
Stephen Odundo <steveremdi@gmail.com>
2021-06-23 12:44:58 +0300

    Add project files.

---
+++ MLPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MLPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
